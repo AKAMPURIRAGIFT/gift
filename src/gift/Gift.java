a3ebe0115fcdb02bef495dce1fd5909bafa61c96
Akampurira Gift <giftakampurira08@gmail.com>
2022-11-02 16:55:17 +0300

    i have added a file

---
+++ src/gift/Gift.java
@@ -0,0 +1,15 @@
+package gift;
+
+public class Gift {
+	public static void main(String[] args ){
+		 System.out.printf("AKAMPURIRA GIFT\n"); 
+		 System.out.printf("BIT\n");  
+		 System.out.printf("20 YRS OLD"); 
+		 System.out.printf("\"JADE\""); 
+		 System.out.printf("My name\n my course\n my district");
+		 
+
+		 
+	}
+	
+}
